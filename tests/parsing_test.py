b1089d6e92e1da68bb4acbf69a27b00c938cf834
Victoria Luquet <victoria.luquet@gdstecnologia.com.br>
2024-04-23 20:19:21 -0300

    test fixing (what)

--- tests/parsing_test.py
+++ tests/parsing_test.py
@@ -1,6 +1,6 @@
 from env_parser import config
 from .results.parsing_test import results
-from env_parser.errors import EnvNotFoundError, DuplicateEnvVariableError, InvalidEnvValueError
+from env_parser.errors import InvalidEnvValueError, DuplicateEnvVariableError, EnvNotFoundError
 import pytest
 import os
 
