1d01650aae5a26824b320944da4d96e8303b9893
Victoria Luquet <victoria.luquet@gdstecnologia.com.br>
2024-04-23 20:03:23 -0300

    Finished multiple environs + tested

--- tests/parsing_test.py
+++ tests/parsing_test.py
@@ -1,23 +1,22 @@
 from env_parser import config
 from .results.parsing_test import results
-from env_parser.errors import InvalidEnvValueError, EnvNotFoundError
+from env_parser.errors import InvalidEnvValueError, EnvNotFoundError, DuplicateEnvVariableError
 import pytest
 import os
 
 class TestParsing:
     def test_parse_and_non_required_value(self, test_id = 0):
-        config_path = './test.yml'
+        config_path = './templates/single_env.yml'
         os.environ['REDIS_URL'] = 'redis://localhost:8003'
         os.environ['MAX_TIME'] = '2'
 
         envs = config(config_path)
         os.environ.pop('REDIS_URL')
         os.environ.pop('MAX_TIME')
-        
         assert envs == results[test_id]
     
     def test_all_values_convertion(self, test_id = 1):
-        config_path = './test.yml'
+        config_path = './templates/single_env.yml'
         os.environ['REDIS_URL'] = 'redis://localhost:8003'
         os.environ['MAX_TIME'] = '4'
         os.environ['THRESH'] = '0.25'
@@ -31,7 +30,7 @@ class TestParsing:
         assert envs == results[test_id]
     
     def test_missing_required_error(self):
-        config_path = './test.yml'
+        config_path = './templates/single_env.yml'
         
         os.environ['REDIS_URL'] = 'redis://localhost:8003'
         os.environ['THRESH'] = '0.25'
@@ -41,7 +40,7 @@ class TestParsing:
             config(config_path)
     
     def test_value_not_inside_options(self):
-        config_path = './test.yml'
+        config_path = './templates/single_env.yml'
         
         os.environ['REDIS_URL'] = 'redis://localhost:8003'
         os.environ['MAX_TIME'] = '6'
@@ -49,4 +48,15 @@ class TestParsing:
         os.environ['DEV_MODE'] = 'true'
         
         with pytest.raises(InvalidEnvValueError) as err:
+            config(config_path)
+    
+    def test_duplicate_env_var(self):
+        config_path = './templates/single_env_duplicate.yml'
+        
+        os.environ['REDIS_URL'] = 'redis://localhost:8003'
+        os.environ['MAX_TIME'] = '2'
+        os.environ['THRESH'] = '0.25'
+        os.environ['DEV_MODE'] = 'true'
+        
+        with pytest.raises(DuplicateEnvVariableError) as err:
             config(config_path)
--- tests/results/parsing_test.py
+++ tests/results/parsing_test.py
@@ -8,5 +8,21 @@ results = [
         'MAX_TIME': 4,
         'THRESH': 0.25,
         'DEV_MODE': True
+    },
+    {
+        'development': {
+            'REDIS_URL': 'redis://localhost:8003',
+            'MAX_TIME': 2,
+            'THRESH': 0.25,
+        },
+        'staging': {
+            'REDIS_URL': 'redis://localhost:6937',
+            'MAX_TIME': 2,
+            'THRESH': 0.5,
+        },
+        'production': {
+            'REDIS_URL': 'redis://app.azure.game:6937',
+            'MAX_TIME': 5,
+        },
     }
 ]
